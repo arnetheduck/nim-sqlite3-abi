04f5d5c96a9d35d067745db7c73baf3b062b9076
Jacek Sieka <jacek@status.im>
2020-02-17 17:16:01 +0100

    add include dir

--- sqlite3_abi.nim
+++ sqlite3_abi.nim
@@ -4,4 +4,8 @@
 import sqlite3_gen
 export sqlite3_gen
 
+# Needed on windows
+from os import splitPath
+{.passC:"-I" & currentSourcePath().splitPath.head .}
+
 proc sqlite3_bind_blob*(pstmt: ptr sqlite3_stmt, param: cint, value: pointer, n: cint, dispose: proc (v: pointer) {.cdecl.}): cint {.importc, header: "sqlite3.h", cdecl.}
