4a71ad6160754f913e0cd303a35122ad0fd21434
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2021-01-10 21:13:24 +0200

    Update to 3.34

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 MAJOR="${1:-3}"
-MINOR="${2:-27}"
-PATCH="${3:-2}"
+MINOR="${2:-34}"
+PATCH="${3:-0}"
 
 VER_INT="$(printf "%d%02d%02d00" "$MAJOR" "$MINOR" "$PATCH")"
 
 # TODO check sha256sum
 
 ZIP="sqlite-amalgamation-$VER_INT.zip"
-[ -f "$ZIP" ] || wget https://www.sqlite.org/2019/$ZIP
+[ -f "$ZIP" ] || wget https://www.sqlite.org/2020/$ZIP
 
 unzip -jo $ZIP "sqlite-amalgamation-$VER_INT/sqlite3.c" "sqlite-amalgamation-$VER_INT/sqlite3.h"
 
