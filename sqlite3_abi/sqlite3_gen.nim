417e2c3997dec04b7f95e9bedb48115775596119
Etan Kissling <etan@status.im>
2024-02-23 13:18:49 +0100

    bump `sqlite-amalgamation` to `3.43.1`
    - https://www.sqlite.org/releaselog/3_43_1.html

--- sqlite3_abi/sqlite3_gen.nim
+++ sqlite3_abi/sqlite3_gen.nim
@@ -27,9 +27,9 @@ else:
   {.pragma: sqlitedecl, cdecl, gcsafe, raises: [].}
 {.compile: "sqlite3_abi/sqlite3.c".}
 const
-  SQLITE_VERSION* = "3.43.0"
-  SQLITE_VERSION_NUMBER* = 3043000
-  SQLITE_SOURCE_ID* = "2023-08-24 12:36:59 0f80b798b3f4b81a7bb4233c58294edd0f1156f36b6ecf5ab8e83631d468778c"
+  SQLITE_VERSION* = "3.43.1"
+  SQLITE_VERSION_NUMBER* = 3043001
+  SQLITE_SOURCE_ID* = "2023-09-11 12:01:27 2d3a40c05c49e1a49264912b1a05bc2143ac0e7c3df588276ce80a4cbc9bd1b0"
   SQLITE_OK* = 0
   SQLITE_ERROR* = 1
   SQLITE_INTERNAL* = 2
--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ git diff --exit-code -- . ':(exclude)update.sh' > /dev/null || { echo "Commit ch
 # https://www.sqlite.org/download.html
 MAJOR="${1:-3}"
 MINOR="${2:-43}"
-PATCH="${3:-0}"
+PATCH="${3:-1}"
 YEAR="${4:-2023}"
-HASH="${5:-1286856f5dff3b145f02dfe5aa626657c172a415889e5a265dc4dee83a799c03}"
+HASH="${5:-2110f85a90033016f5b5691812448e8dfa606fe407f4b2ebfec66dda2968fe3c}"
 
 VER_INT="$(printf "%d%02d%02d00" "$MAJOR" "$MINOR" "$PATCH")"
 
